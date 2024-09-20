72951a4ed781487058fd4779555b9c77d616d3e1
yafiet231 <abieleyasu893@gmail.com>
2024-09-20 13:36:16 +0000

    fix

--- 0x06-unittests_in_js/1-calcul.js
+++ 0x06-unittests_in_js/1-calcul.js
@@ -1,3 +1,14 @@
-const calculateNumber = (a, b) => Math.round(a) + Math.round(b);
+const calculateNumber = (type, a, b) => {
+  if (type === 'SUM') {
+    return Math.round(a) + Math.round(b);
+  }
+  if (type === 'SUBTRACT') {
+    return Math.round(a) - Math.round(b);
+  }
+  if (type === 'DIVIDE') {
+    return Math.round(b) === 0 ? 'Error' : Math.round(a) / Math.round(b);
+  }
+  return 0;
+};
 
 module.exports = calculateNumber;
